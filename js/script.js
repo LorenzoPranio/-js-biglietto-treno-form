dc773f0bcabe7430761eaacfa89d5409b7d4d755
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-26 17:22:58 +0200

    [UPD]Added IF and prezzo finale in script.js, finished milestone 1

--- js/script.js
+++ js/script.js
@@ -4,5 +4,15 @@ calculate.addEventListener('click', function (){
     let kilometri = document.getElementById('km').value;
     let age = document.getElementById('age').value;
     let prezzo_biglietto = 0.21 * kilometri;
-
+    if (age == 'maggiorenne'){
+        sconto = 0;
+    }
+    else if (age == 'minorenne'){
+        sconto = 20;
+    }
+    else if (age == 'over65'){
+        sconto = 40;
+    }
+    let prezzo_finale = prezzo_biglietto - (prezzo_biglietto * sconto / 100)
+    console.log(prezzo_finale.toFixed(2));
 });
