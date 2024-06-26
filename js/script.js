e2c9fff22196639651d5dff3f554cb37cc80e1d6
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-26 17:18:09 +0200

    [UPD]Added eventlistener with kilometri, age and prezzo biglietto in script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let calculate = document.getElementById('calculate');
+
+calculate.addEventListener('click', function (){
+    let kilometri = document.getElementById('km').value;
+    let age = document.getElementById('age').value;
+    let prezzo_biglietto = 0.21 * kilometri;
+
+});
