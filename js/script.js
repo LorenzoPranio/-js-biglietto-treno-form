45c9cc6eec348f0b518fbb83d4b07bd444079571
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-26 18:24:59 +0200

    [UPD]Added functionality of prezzo finale and name-surname in script.js and index.html

--- js/script.js
+++ js/script.js
@@ -4,10 +4,8 @@ calculate.addEventListener('click', function (){
     let kilometri = document.getElementById('km').value;
     let age = document.getElementById('age').value;
     let prezzo_biglietto = 0.21 * kilometri;
-    if (age == 'maggiorenne'){
-        sconto = 0;
-    }
-    else if (age == 'minorenne'){
+    let sconto = 0;
+    if (age == 'minorenne'){
         sconto = 20;
     }
     else if (age == 'over65'){
@@ -15,6 +13,14 @@ calculate.addEventListener('click', function (){
     }
     let prezzo_finale = prezzo_biglietto - (prezzo_biglietto * sconto / 100)
     console.log(prezzo_finale.toFixed(2));
+
+    let name_surname = document.getElementById('name').value;
+
+    document.getElementById('name-surname').innerHTML = name_surname;
+
+    console.log(name_surname)
+
+    document.getElementById('prezzo-finale').innerHTML = prezzo_finale.toFixed(2) + '&euro;'
 });
 
 let btndelete = document.getElementById('delete');
@@ -22,3 +28,4 @@ let btndelete = document.getElementById('delete');
 btndelete.addEventListener('click', function(){
     document.getElementById('form').reset();
 })
+
